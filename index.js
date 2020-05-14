032e33cf0f1096f81795bef39c291e3594ee4cd6
ngocphucdo <taken.jb@gmail.com>
2020-05-14 22:33:36 +0700

    edit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const scraper = require("./scraping");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
@@ -21,7 +23,7 @@ app.get("/movie/:imdbID", (req, res) => {
   });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.port || 8080;
 
 app.listen(port, (err) => {
   if (err) {
